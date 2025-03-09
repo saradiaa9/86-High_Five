94f0ddd49326f335f0bd5baef8db1298b0b19422
Kareem Hegazy <kareemhegazy256@gmail.com>
2025-03-09 16:33:50 +0200

    Kareem First and Last Commit Inshallah.

--- src/main/java/com/example/model/Order.java
+++ src/main/java/com/example/model/Order.java
@@ -1,5 +1,43 @@
 package com.example.model;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.stereotype.Component;
+
+@Component
 public class Order {
-    
+	private UUID id;
+	private UUID userId;
+	private double totalPrice;
+	private final List<Product> products = new ArrayList<>();
+
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+	public UUID getUserId() {
+		return userId;
+	}
+
+	public void setUserId(UUID userId) {
+		this.userId = userId;
+	}
+
+	public double getTotalPrice() {
+		return totalPrice;
+	}
+
+	public void setTotalPrice(double totalPrice) {
+		this.totalPrice = totalPrice;
+	}
+
+	public List<Product> getProducts() {
+		return products;
+	}
 }
--- src/main/java/com/example/service/OrderService.java
+++ src/main/java/com/example/service/OrderService.java
@@ -1,5 +1,38 @@
 package com.example.service;
 
-public class OrderService {
+import java.util.ArrayList;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.model.Order;
+import com.example.repository.OrderRepository;
+
+@Service
+@SuppressWarnings("rawtypes")
+public class OrderService extends MainService<Order> {
+
+    @Autowired
+    OrderRepository orderRepository;
+
+    public OrderService() {
+    }
+
+    public void addOrder(Order order) {
+        orderRepository.save(order);
+    }
+
+    public ArrayList<Order> getOrders(){
+        return orderRepository.findAll();
+    }
+
+    public Order getOrderById(UUID orderId){
+        return orderRepository.getOrderById(orderId);
+    }
+
+    public void deleteOrderById(UUID orderId) throws IllegalArgumentException{
+        orderRepository.deleteOrderById(orderId);
+    }
     
 }
